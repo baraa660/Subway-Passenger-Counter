226657074cf5942a653c71f84e6acd646c6464b3
baraa bzour <121990159+baraa660@users.noreply.github.com>
2023-01-17 22:00:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// intialize the count as 0
+let count = 0
+let countEl = document.getElementById("count-el")
+
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+function increment() {
+    count = count + 1
+    countEl.textContent = count;
+
+    
+}
+
+
+
+// change the count-el in the HTML to reflect the new count
