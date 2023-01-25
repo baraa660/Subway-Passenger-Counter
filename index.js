8deb45840c7109c62a39f5e7eda594e1d7c998dd
baraa bzour <121990159+baraa660@users.noreply.github.com>
2023-01-25 15:13:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// intialize the count as 0
-let count = 0
-let countEl = document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+let countEl=document.getElementById("count-el")
+let count =0
 
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-function increment() {
-    count = count + 1
-    countEl.textContent = count;
-
-    
+function increment(){
+    count=count+1
+    countEl.innerText=count
 }
 
-
-
-// change the count-el in the HTML to reflect the new count
+function save(){
+    let countstr= count + " - "
+    saveEl.textContent+=countstr
+    count=0
+    countEl.innerText=count
+}
